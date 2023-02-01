69ce33abd90d57aefc54270b0ecc5b984d5efc88
Len Blum <mail@lenblum.de>
2023-02-01 18:26:35 +0100

    Co-authored-by: Ursus24 <Ursus24@users.noreply.github.com>

--- src/main.go
+++ src/main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"gorm.io/driver/sqlite"
-	_ "gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	_ "gorm.io/gorm"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	_ = db
-	db.AutoMigrate(&Film{})
-	db.Create(&Film{Name: "", IMDB: 12})
-	var film Film
-	db.First(&film, 1) // find product with integer primary key
-	fmt.Println(film)
-	if err != nil {
-		panic("failed to connect database")
-	}
 	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, IP=${remote_ip}, status=${status}, latency:${latency},\n",
+	}))
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Root Route!")
 	})
