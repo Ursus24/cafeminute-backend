b009db14c601bce93ee3aec69c4483f2f67ab363
Ursus24 <urs.jokisch@freenet.de>
2023-02-07 16:48:25 +0100

    began to add notifications

---
+++ src/check_format.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func checkDate(d string) bool {
+	correct := true
+
+	s := strings.Split(d, ("."))
+
+	day, _ := strconv.ParseInt(s[1], 10, 0)
+	month, _ := strconv.ParseInt(s[2], 10, 0)
+	year, _ := strconv.ParseInt(s[3], 10, 0)
+
+	if day > 31 {
+		correct = false
+	}
+
+	if month > 12 {
+		correct = false
+	}
+
+	if year < 2023 {
+		correct = false
+	}
+
+	return correct
+}
+
+func checkTime(d string) bool {
+	correct := true
+
+	s := strings.Split(d, ("."))
+
+	hour, _ := strconv.ParseInt(s[1], 10, 0)
+	minutes, _ := strconv.ParseInt(s[2], 10, 0)
+
+	if hour > 24 {
+		correct = false
+	}
+
+	if minutes > 60 {
+		correct = false
+	}
+
+	return correct
+}
