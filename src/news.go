65258f6411dd0dcb7a800d4681f81230298706e0
Len <mail@lenblum.de>
2023-04-08 10:06:19 +0200

    fixed getallnews

--- src/news.go
+++ src/news.go
@@ -124,6 +124,10 @@ func getallnews(c echo.Context) error {
 		product = append(product, prdct)
 		prdct = fmt.Sprintln(f.Name() + ": " + readKeyUnsafe("image", "news/"+f.Name()+"/") + ";")
 		product = append(product, prdct)
+		prdct = fmt.Sprintln(f.Name() + ": " + readKeyUnsafe("event", "news/"+f.Name()+"/") + ";")
+		product = append(product, prdct)
+		prdct = fmt.Sprintln(f.Name() + ": " + readKeyUnsafe("participants", "news/"+f.Name()+"/") + ";")
+		product = append(product, prdct)
 		joined := strings.Join(product, "")
 		products = append(products, joined)
 	}
